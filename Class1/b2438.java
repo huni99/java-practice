0b150f4af2543e0ff1fd2d39d7b2dcf704bbec9b
jeong gil hun <wjdrlfgns1@naver.com>
2025-05-30 12:12:35 +0900

    2438 추가내용

--- Class1/b2438.java
+++ Class1/b2438.java
@@ -8,13 +8,13 @@ public class b2438 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb =new StringBuilder();
     int num = Integer.parseInt(st.nextToken());
-    
+    String star ="*";
     for(int i=1; i<=num;i++){
-      for(int j =1; j<=i ; j++){
-        System.out.print("*");
+      sb.append(star.repeat(i)).append("\n");
+    
       }
-      System.out.println();
-    }
+      System.out.println(sb);
   }
 }
